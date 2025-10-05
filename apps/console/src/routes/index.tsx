bba8fb12ea7128d5da514f14aaa8c36c1519ea1e
titanium-dev <akinkunmioye42@gmail.com>
2025-10-05 02:42:01 +0100

    feat(console): update login UI with improved styling and layout

--- apps/console/src/routes/index.tsx
+++ apps/console/src/routes/index.tsx
@@ -10,10 +10,12 @@ function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="flex flex-col gap-y-6 items-center">
-        <h1 className="text-4xl">Login to {APP_NAME}</h1>
-        <button className="bg-accent text-white rounded-full flex items-center p-2 gap-x-2 transition-shadow duration-200 hover:shadow-[0_0_16px_4px_rgba(244,88,23,0.6)]">
-          <SiGithub />
-          <span className="uppercase text-lg">Sign in with GitHub</span>
+        <h1 className="text-xl uppercase">{APP_NAME}</h1>
+        <button className="flex items-center border border-gray-400 h-8 gap-x-2">
+          <span className="bg-white/10 h-full aspect-square flex items-center justify-center px-2">
+            <SiGithub />
+          </span>
+          <span className="uppercase text-xs pr-2">Sign in with GitHub</span>
         </button>
       </div>
     </div>
